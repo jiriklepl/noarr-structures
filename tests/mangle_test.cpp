8dfb1aa052febc8620a7c900bcec38fe27c012e0
Jiří Klepl <klepl@d3s.mff.cuni.cz>
2026-08-18 23:31:32 +0200

    fix the mangle test
    - add the missing `<cstdint>` header
    - add `const` to constant variables

--- tests/mangle_test.cpp
+++ tests/mangle_test.cpp
@@ -1,6 +1,7 @@
 #include <noarr_test/macros.hpp>
 
 #include <cstddef>
+#include <cstdint>
 
 #include <string>
 #include <type_traits>
@@ -12,8 +13,8 @@
 template<char... Chars>
 static std::string cp2a(std::integer_sequence<char, Chars...> /*is*/) { return {Chars...}; }
 
-static std::string int_name = "int" + std::to_string(sizeof(int) * 8) + "_t";
-static std::string size_name = "uint" + std::to_string(sizeof(std::size_t) * 8) + "_t";
+static const std::string int_name = "int" + std::to_string(sizeof(int) * 8) + "_t";
+static const std::string size_name = "uint" + std::to_string(sizeof(std::size_t) * 8) + "_t";
 
 TEST_CASE("Mangle scalar", "[mangle]") {
 	using s = noarr::scalar<int>;
@@ -31,13 +32,13 @@ TEST_CASE("Mangle array", "[mangle]") {
 
 TEST_CASE("Mangle expr", "[mangle]") {
 	using namespace noarr;
-	auto &sn = size_name; // shorten name for alignment
+	const auto &sn = size_name; // shorten name for alignment
 
-	auto structure =        set_length_t<'y',vector_t<'y',set_length_t<'x',vector_t<'x',scalar<int32_t>>,std::integral_constant<size_t,42>>>,size_t>{vector_t<'y',set_length_t<'x',vector_t<'x',scalar<int32_t>>,std::integral_constant<size_t,42>>>{set_length_t<'x',vector_t<'x',scalar<int32_t>>,std::integral_constant<size_t,42>>{vector_t<'x',scalar<int32_t>>{scalar<int32_t>{},},lit<42>,},},size_t{24},};
-	std::string expected = "set_length_t<'y',vector_t<'y',set_length_t<'x',vector_t<'x',scalar<int32_t>>,std::integral_constant<"+sn+",42>>>,"+sn+">{vector_t<'y',set_length_t<'x',vector_t<'x',scalar<int32_t>>,std::integral_constant<"+sn+",42>>>{set_length_t<'x',vector_t<'x',scalar<int32_t>>,std::integral_constant<"+sn+",42>>{vector_t<'x',scalar<int32_t>>{scalar<int32_t>{},},lit<42>,},},"+sn+"{24},}";
-	std::string actual = noarr::mangle_expr<std::string>(structure);
+	const auto structure =        set_length_t<'y',vector_t<'y',set_length_t<'x',vector_t<'x',scalar<int32_t>>,std::integral_constant<size_t,42>>>,size_t>{vector_t<'y',set_length_t<'x',vector_t<'x',scalar<int32_t>>,std::integral_constant<size_t,42>>>{set_length_t<'x',vector_t<'x',scalar<int32_t>>,std::integral_constant<size_t,42>>{vector_t<'x',scalar<int32_t>>{scalar<int32_t>{},},lit<42>,},},size_t{24},};
+	const std::string expected = "set_length_t<'y',vector_t<'y',set_length_t<'x',vector_t<'x',scalar<int32_t>>,std::integral_constant<"+sn+",42>>>,"+sn+">{vector_t<'y',set_length_t<'x',vector_t<'x',scalar<int32_t>>,std::integral_constant<"+sn+",42>>>{set_length_t<'x',vector_t<'x',scalar<int32_t>>,std::integral_constant<"+sn+",42>>{vector_t<'x',scalar<int32_t>>{scalar<int32_t>{},},lit<42>,},},"+sn+"{24},}";
+	const std::string actual = noarr::mangle_expr<std::string>(structure);
 
-	[[maybe_unused]] auto structure_pretty = noarr::array_t<'x', 42, noarr::scalar<int>>() ^ noarr::vector<'y'>() ^ noarr::set_length<'y'>(24);
+	[[maybe_unused]] const auto structure_pretty = noarr::array_t<'x', 42, noarr::scalar<int>>() ^ noarr::vector<'y'>() ^ noarr::set_length<'y'>(24);
 	static_assert(std::is_same_v<decltype(structure), decltype(structure_pretty)>, "Test sanity check");
 
 	REQUIRE(expected == actual);
